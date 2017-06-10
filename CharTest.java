b2d854650c12029ceec5d2e5a9ad1b8e579e5a39
Kuanwei1225 <future31415926@gmail.com>
2017-06-10 16:48:37 +0800

    Add files via upload

---
+++ CharTest.java
@@ -0,0 +1,14 @@
+/**
+ * Created by K on 2016/4/3.
+ */
+public class CharTest {
+    public static void main(String[] args) {
+        int a = 9, b = 10, c = 11;
+        byte ch1 = (byte)a;
+        byte ch2 = (byte)b;
+        byte ch3 = (byte)c;
+        System.out.println((int)ch1);
+        System.out.println((int)ch2);
+        System.out.println((int)ch3);
+    }
+}
